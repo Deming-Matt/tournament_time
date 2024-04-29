3fc9c491f5688a510304e51d5306841903017981
Matt Deming <vballmatt44@gmail.com>
2024-04-29 16:46:24 -0600

    Created a player/user model and test suite

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_29_034137) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_29_040342) do
   create_table "teams", force: :cascade do |t|
     t.string "name"
     t.string "captain"
@@ -24,6 +24,13 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_29_034137) do
     t.string "host"
     t.string "location"
     t.integer "number_of_teams"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
